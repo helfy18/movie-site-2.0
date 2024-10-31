2b2407eb9ed387707019c694e4b8a56fa13d8a34
Johnathan Helfrich <johnathan@19labs.com>
2024-10-31 09:37:40 -0700

    node package update

--- src/components/infoTable.tsx
+++ src/components/infoTable.tsx
@@ -5,84 +5,43 @@ interface MovieGridProps {
 }
 
 const InfoTable = ({ movie }: MovieGridProps) => {
+  const rows = [
+    { label: "Title", value: movie.movie },
+    {
+      label: "Score",
+      value: `${movie.jh_score}/100`,
+      style: { color: gradient[movie.jh_score], fontWeight: "bolder" },
+    },
+    { label: "Universe", value: movie.universe },
+    { label: "Sub Universe", value: movie.sub_universe },
+    {
+      label: "Genres",
+      value: `${movie.genre}${movie.genre_2 ? `, ${movie.genre_2}` : ""}`,
+    },
+    { label: "Exclusive", value: movie.exclusive },
+    { label: "Holiday", value: movie.holiday },
+    { label: "Year", value: movie.year },
+    { label: "MPA Rating", value: movie.rated },
+    { label: "Runtime", value: `${movie.runtime} min` },
+    { label: "Budget", value: `$${movie.budget}` },
+    { label: "Box Office", value: `$${movie.boxoffice}` },
+    { label: "Actors", value: movie.actors },
+    { label: "Director", value: movie.director },
+    { label: "Studio", value: movie.studio },
+  ];
+
   return (
     <table id="infoTable" className="table">
       <tbody>
-        <tr>
-          <td>Title</td>
-          <td>{movie.movie}</td>
-        </tr>
-        <tr>
-          <td>Score</td>
-          <td
-            style={{
-              color: gradient[movie.jh_score],
-              fontWeight: "bolder",
-            }}
-          >
-            {`${movie.jh_score}/100`}
-          </td>
-        </tr>
-        {movie.universe && (
-          <tr>
-            <td>Universe</td>
-            <td>{movie.universe}</td>
-          </tr>
+        {rows.map(
+          ({ label, value, style }) =>
+            value && (
+              <tr key={label}>
+                <td>{label}</td>
+                <td style={style}>{value}</td>
+              </tr>
+            )
         )}
-        {movie.sub_universe && (
-          <tr>
-            <td>Sub Universe</td>
-            <td>{movie.sub_universe}</td>
-          </tr>
-        )}
-        <tr>
-          <td>Genres</td>
-          <td>{`${movie.genre}${movie.genre_2 && `, ${movie.genre_2}`}`}</td>
-        </tr>
-        {movie.exclusive && (
-          <tr>
-            <td>Exclusive</td>
-            <td>{movie.exclusive}</td>
-          </tr>
-        )}
-        {movie.holiday && (
-          <tr>
-            <td>Holiday</td>
-            <td>{movie.holiday}</td>
-          </tr>
-        )}
-        <tr>
-          <td>Year</td>
-          <td>{movie.year}</td>
-        </tr>
-        <tr>
-          <td>MPA Rating</td>
-          <td>{movie.rated}</td>
-        </tr>
-        <tr>
-          <td>Runtime</td>
-          <td>{movie.runtime} min</td>
-        </tr>
-        <tr>
-          <td>Budget</td>
-          <td>${movie.budget}</td>
-        </tr>
-        <tr>
-          <td>Box Office</td>
-          <td>${movie.boxoffice}</td>
-        </tr>
-        <tr>
-          <td>Actors</td>
-          <td>{movie.actors}</td>
-        </tr>
-        <tr>
-          <td>Director</td>
-          <td>{movie.director}</td>
-        </tr>
-        <tr>
-          <td>Studio</td>
-          <td>{movie.studio}</td>
-        </tr>
       </tbody>
     </table>
   );
